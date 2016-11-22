31c9c746bc18506ee8a08923ba915bca0c3185f3
SThor <paulgivel@gmail.com>
2016-11-22 09:15:46 +0100

    login-generator-2 Couvrir LoginGenerator à 100% par des tests unitaires #2
    fix #3

--- src/main/java/geco/LoginGenerator.java
+++ src/main/java/geco/LoginGenerator.java
@@ -36,9 +36,12 @@ public class LoginGenerator {
     public String generateLoginForNomAndPrenom(String nom, String prenom) {
         String p = deAccent(prenom.substring(0,1).toUpperCase());
         String n = deAccent(nom.substring(0,3).toUpperCase());
-        String login = p+n ;
-        if (loginService.loginExists(login)) {
-            login = login + "1" ;
+        String baseLogin = p+n ;
+        String login = baseLogin;
+        int inc = 0;
+        while (loginService.loginExists(login)) {
+            inc++;
+            login = baseLogin + inc ;
         }
         loginService.addLogin(login);
         return login;
